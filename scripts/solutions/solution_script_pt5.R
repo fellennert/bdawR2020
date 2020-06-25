f48385e954d04a2dd13c4c72170fe18914ad8c45
fellennert <felix.lennert@icloud.com>
2020-06-25 23:24:33 +0200

    new exercises

--- scripts/solutions/solution_script_pt5.R
+++ scripts/solutions/solution_script_pt5.R
@@ -28,3 +28,28 @@ imdb %>% filter(year == 2016) %>% arrange(desc(revenue_million))
 # 6
 imdb %>% group_by(year) %>% 
   summarize(rev_sum = sum(revenue_million, na.rm = TRUE)*1000000)
+
+# 7. Filter movies following some conditions:
+    # a. More runtime than the average runtime (hint: you could also use `mutate()` before).
+imdb %>% filter(runtime > mean(runtime))
+
+    b. Movies directed by J. J. Abrams.
+imdb %>% filter(director == "J.J. Abrams")
+    c. More votes than the median of all of the votes.
+imdb %>% filter(votes > median(votes))
+    d. The movies which have the most common value in terms of rating (`mode()` does not exist -- run the script below and use the `my_mode` function).
+imdb %>%  filter(rating == my_mode(rating))
+
+# function
+my_mode <- function(x){ 
+    ta = table(x)
+    tam = max(ta)
+    if (all(ta == tam))
+         mod = NA
+    else
+         if(is.numeric(x))
+    mod = as.numeric(names(ta)[ta == tam])
+    else
+         mod = names(ta)[ta == tam]
+    return(mod)
+} 
